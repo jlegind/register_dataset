6f1e66e1c36b1a795389f4f2cd89589a4eaf40a0
Jan Legind <jlegind@gbif.org>
2016-11-01 14:22:30 +0100

    code cleaned up

--- registerdatasets.py
+++ registerdatasets.py
@@ -10,8 +10,11 @@ class RegisterDataset(object):
     """Gets the dataset titles and gets the datasets of type 'occurrence' only"""
     def __init__(self, url_list):
         self.res = []
-        for url in url_list:
-            self.res.append(requests.get(url))
+        try:
+            for url in url_list:
+                self.res.append(requests.get(url))
+        except requests.exceptions.MissingSchema as e:
+            print('\n !!!', e)
 
 
     def gettitle(self, res):
@@ -32,6 +35,10 @@ class RegisterDataset(object):
         return title
 
     def gettype(self, res):
+        '''
+        :param res: A list of URL objects
+        :return: The type of dataset either OCCURRENCE or SAMPLING EVENT
+        '''
         pattern = re.compile("meta\.xml$")
         dwc_type = []
         for j in res:
@@ -65,21 +72,39 @@ class RegisterDataset(object):
 class CreateDataset(object):
     """Creates  datasets in the GBIF registry and crawls them"""
     def __init__(self, header, payload, title, type, user, pw, api_url='http://api.gbif.org/v1/dataset'):
+        '''
+        :param header: Metadata for the API call. JSON expected.
+        :param payload: Parameters going into the API call (installation key and publisher key)
+        :param title: Dataset title
+        :param type: Dataset type
+        :param user: GBIF user name
+        :param pw: Password to the GBIF account
+        :param api_url: The key word arg
+        '''
         self.api_url = api_url
         self.header = header
         payload['title'] = title
         payload['type'] = type
         print(payload)
+        self.payload = payload
         self.credentials = (user, pw)
-        self.res = requests.post(api_url, auth=self.credentials, data=json.dumps(payload), headers=header).text
+
+
+    def create_dataset(self):
+        #Creates the global res var that contains the dataset UUID
+        self.res = requests.post(self.api_url, auth=self.credentials, data=json.dumps(self.payload), headers=self.header).text
         self.res = self.res.strip('"')
         print(self.res)
 
 
     def create_endpoint(self, dataset_uuid, endpoint):
+        '''
+        :param dataset_uuid: Dataset key (GBIF UUID)
+        :param endpoint: Dataset DwC archive endpoint
+        :return: Endpoint ID
+        '''
         create_endpoint_url = '{}/{}/endpoint'.format(self.api_url, dataset_uuid)
         print(create_endpoint_url)
-        print(endpoint, "TEST")
         jsn = {"type": "DWC_ARCHIVE", "url": endpoint}
         r = requests.post(create_endpoint_url, auth=self.credentials, data=json.dumps(jsn), headers=self.header)
         print(r.text)
@@ -87,28 +112,10 @@ class CreateDataset(object):
 
 
     def crawl_dataset(self, uuid):
-        crawlurl = '{}/v1/dataset/{}/crawl'.format(self.api_url, uuid)
+        '''
+        :param uuid: Dataset to be crawled
+        '''
+        crawlurl = '{}/{}/crawl'.format(self.api_url, uuid)
         print(crawlurl)
+        print(self.credentials)
         requests.post(crawlurl, auth=self.credentials)
-
-##TEST CODE##
-# myurl = 'http://asnhc.angelo.edu/archives/'
-# archive = ['http://asnhc.angelo.edu/archives/dwca-angelo-reptiles.zip', 'http://asnhc.angelo.edu/archives/dwca-angelo-plants.zip']
-# chk = CheckDataset(archive)
-# titles = chk.gettitle(chk.res)
-# titles_pop = titles[:]
-# #essentially makes a copy
-# print(titles)
-# types = chk.gettype(chk.res)
-# print(types)
-#
-# header = {'Content-Type': 'application/json'}
-# payload = {"installationKey":"afafe88e-4b8e-4e62-8f38-3eaa24f71532","publishingOrganizationKey":"9c0a8aa8-4ce7-49ba-aac7-21a97234f886","title":None,
-#              "type":None}
-# uat_api = 'http://api.gbif-uat.org/v1/dataset'
-#
-# for j in titles:
-#     mydataset = CreateDataset(header, payload, titles_pop.pop(), types.pop(), 'jlegind', 'mussimus', api_url=uat_api)
-#     mydataset.create_endpoint(mydataset.res, archive.pop())
-#     mydataset.crawl_dataset(mydataset.res)
-## ##
